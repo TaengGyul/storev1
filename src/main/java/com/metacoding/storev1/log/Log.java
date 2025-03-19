6f716639109b35fc23ba2ef07bf9603737b3cb72
HaEun <mjcho0130@gmail.com>
2025-03-19 17:16:40 +0900

    view complete

---
+++ src/main/java/com/metacoding/storev1/log/Log.java
@@ -0,0 +1,25 @@
+package com.metacoding.storev1.log;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // JPA가 ObjectMapping을 위해 new 할 때 사용함.
+@Table(name = "log_tb")
+@Entity // 설정 파일에서 테이블을 생성해 준다.
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer storeId; // 상품 ID (FK)
+    private Integer qty; // 구매 개수
+    private Integer totalPrice; // qty*store(price)
+    private String buyer; // 구매자 이름
+
+}
